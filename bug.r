8d5ed57e1cfd911d6784e06a84017c53fa4999b4
BugHunter-X <askzenai@gmail.com>
2025-01-05 16:58:05 +0100

    Add bug.r file

---
+++ bug.r
@@ -0,0 +1,6 @@
+```r
+# This R code attempts to perform a calculation involving a large number and then prints the result.
+num <- 10^1000  # A very large number
+result <- num / 2
+print(result)
+```
